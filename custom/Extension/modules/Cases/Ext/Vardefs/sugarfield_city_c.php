c62032fca20069e9f21921e99399bc1570133641
Talha Shakil <talha.shakil@rolustech.net>
2022-07-14 16:28:45 +0500

    City Field Added

---
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_city_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2022-07-14 16:01:47
+$dictionary['Case']['fields']['city_c']['labelValue']='city';
+$dictionary['Case']['fields']['city_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Case']['fields']['city_c']['enforced']='';
+$dictionary['Case']['fields']['city_c']['dependency']='';
+$dictionary['Case']['fields']['city_c']['required_formula']='';
+$dictionary['Case']['fields']['city_c']['readonly_formula']='';
+
+ ?>
