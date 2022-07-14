b7df98bc4056862e1ed0654ebec6bfa412a44b41
Talha Shakil <talha.shakil@rolustech.net>
2022-07-14 16:49:26 +0500

    Phone Number Field Added

---
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_phone_number_c.php
@@ -0,0 +1,17 @@
+<?php
+$fields=array(
+	'name' => 'integer_field_example',
+	'label' => 'LBL_INTEGER_FIELD_EXAMPLE',
+	'type' => 'INTEGER',
+	'module' => 'Accounts',
+	'help' => '',
+	'comment' => '',
+	'default_value' => 13,
+	'max_size' => 255,
+	'required' => false, // true or false
+	'reportable' => true, // true or false
+	'audited' => false, // true or false
+	'importable' => 'true', // 'true', 'false', 'required'
+	'duplicate_merge' => false, // true or false
+)
+ ?>
